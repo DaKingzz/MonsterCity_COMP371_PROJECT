06aca6288ca835d100d6ca812be012d77bc77cab
Pai <pphaoniubi@gmail.com>
2025-08-01 11:49:00 -0400

    further cleanup

--- Project371.cpp
+++ Project371.cpp
@@ -24,6 +24,7 @@
 #include "shaders.h"
 #include "texture.h"
 #include "vertex.h"
+#include "geometry.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
@@ -31,34 +32,6 @@
 using namespace glm;
 using namespace std;
 
-quat rotationBetweenVectors(vec3 start , vec3 dest){
-    start = normalize(start);
-    dest = normalize(dest);
-
-    float cosTheta = dot(start , dest);
-    vec3 rotationAxis ;
-    
-    if(cosTheta < -0.999f){
-        rotationAxis = cross(vec3(0.0f , 0.0f, 1.0f),start);
-        if(length(rotationAxis) < 0.01f)
-        rotationAxis = cross(vec3(1.0f, 0.0f, 0.0f),start);
-        return angleAxis ( glm::radians(180.0f), normalize(rotationAxis));
-    }
-
-    rotationAxis = cross(start,dest);
-    float s = sqrt((1 + cosTheta) *2);
-    float invs = 1.0f /s;
-
-    return quat( s * 0.5f, rotationAxis.x * invs,rotationAxis.y * invs, rotationAxis.x * invs);
-}
-
-
-int createTexturedCubeVertexArrayObject();
-
-
- float GenerateRandomFloat(float min , float max){
-        return min + static_cast<float>(rand())/(static_cast<float>(RAND_MAX/(max -min)));
-    }
 
 
 int main(int argc, char*argv[])
@@ -478,48 +451,4 @@ int main(int argc, char*argv[])
     glfwTerminate();
     
 	return 0;
-}
-
-int createTexturedCubeVertexArrayObject()
-{
-    // Create a vertex array
-    GLuint vertexArrayObject;
-    glGenVertexArrays(1, &vertexArrayObject);
-    glBindVertexArray(vertexArrayObject);
-    
-    // Upload Vertex Buffer to the GPU, keep a reference to it (vertexBufferObject)
-    GLuint vertexBufferObject;
-    glGenBuffers(1, &vertexBufferObject);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
-    glBufferData(GL_ARRAY_BUFFER, texturedCubeVertexArrayCount * sizeof(TexturedColoredVertex), texturedCubeVertexArray, GL_STATIC_DRAW);
-    
-    glVertexAttribPointer(0,                   // attribute 0 matches aPos in Vertex Shader
-                          3,                   // size
-                          GL_FLOAT,            // type
-                          GL_FALSE,            // normalized?
-                          sizeof(TexturedColoredVertex), // stride - each vertex contain 2 vec3 (position, color)
-                          (void*)0             // array buffer offset
-                          );
-    glEnableVertexAttribArray(0);
-    
-    
-    glVertexAttribPointer(1,                            // attribute 1 matches aColor in Vertex Shader
-                          3,
-                          GL_FLOAT,
-                          GL_FALSE,
-                          sizeof(TexturedColoredVertex),
-                          (void*)sizeof(vec3)      // color is offseted a vec3 (comes after position)
-                          );
-    glEnableVertexAttribArray(1);
-    
-    glVertexAttribPointer(2,                            // attribute 2 matches aUV in Vertex Shader
-                          2,
-                          GL_FLOAT,
-                          GL_FALSE,
-                          sizeof(TexturedColoredVertex),
-                          (void*)(2*sizeof(vec3))      // uv is offseted by 2 vec3 (comes after position and color)
-                          );
-    glEnableVertexAttribArray(2);
-    
-    return vertexArrayObject;
 }
